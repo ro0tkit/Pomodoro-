c6442e8c4b65387274a3848818c0f470bb67b356
Luther <lutherking30@gmail.com>
2021-11-11 15:19:43 +0300

    Add email validation

--- app/main/forms.py
+++ app/main/forms.py
@@ -11,7 +11,10 @@ class RegistrationForm(FlaskForm):
     password_confirm = PasswordField('Confirm Password',validators = [Required()])
     submit = SubmitField('Sign Up')
 
- 
+    def validate_email(self,data_field):
+            if User.query.filter_by(email =data_field.data).first():
+                raise ValidationError('email already exists')
+
     def validate_username(self,data_field):
         if User.query.filter_by(username = data_field.data).first():
             raise ValidationError('username is taken')
