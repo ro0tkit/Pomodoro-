f6f9afe855373844b52307dbfd33a1abaa956f64
0724654276 <you@example.com>
2021-11-11 15:32:40 +0300

    created user model

---
+++ app/models.py
@@ -0,0 +1,49 @@
+from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+from datetime import datetime
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(UserMixin, db.Model):
+    '''
+    Model class/db table for the user
+    Args:
+        db.Model: Connect our class to the database
+    '''
+    __tablename__  = 'users'
+
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255), unique = True, index=True)
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    pass_secure = db.Column(db.String())
+    task = db.relationship('task', backref='user', lazy='dynamic')
+
+    @property
+    def password(self):
+        '''
+        Define property object to make limit access to pass_secure
+        '''
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self,password):
+        self.pass_secure = generate_password_hash(password)
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+    def save_user(self):
+        '''
+        Function to save a user
+        '''
+        db.session.add(self)
+        db.session.commit()
+    
+    def __repr__(self):
+        return f'User {self.username}'
